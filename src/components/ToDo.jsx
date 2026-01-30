1520bf0fe11d1610cce3d775bb61b58c867aa677
DalalYouness <155886084+DalalYouness@users.noreply.github.com>
2026-01-30 18:13:00 +0100

    adding style

--- src/components/ToDo.jsx
+++ src/components/ToDo.jsx
@@ -1,9 +1,35 @@
 import React from "react";
 
 const ToDo = ({ todo }) => {
+  const styleTask = {
+    textDecoration: todo.isCompleted ? "line-through" : "none",
+    border: "1px solid black",
+    borderRadius: "5px",
+    width: "300px",
+    padding: "5px",
+    margin: "10px 0",
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    backgroundColor: "#4a62a5ff",
+    fontWeight: "bold",
+    color: "white",
+  };
+  const styleButtons = {
+    margin: "0 5px",
+    outline: "none",
+    border: "none",
+    borderRadius: "5px",
+    padding: "5px",
+    cursor: "pointer",
+  };
   return (
-    <div>
+    <div style={styleTask}>
       <p>{todo.task}</p>
+      <div>
+        <button style={styleButtons}>complete</button>
+        <button style={styleButtons}>edit</button>
+      </div>
     </div>
   );
 };
